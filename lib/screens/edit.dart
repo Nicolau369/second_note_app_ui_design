755ac3a3f6759297f816d042219d51ddb1addea0
1rma0lh0 <1rma0lh0@protonmail.com>
2023-10-19 13:51:29 -0300

    Initial commit

---
+++ lib/screens/edit.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/models/note.dart';
+
+class EditScreen extends StatefulWidget {
+  final Note? note;
+  const EditScreen({super.key, this.note});
+
+  @override
+  State<EditScreen> createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen> {
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _contentController = TextEditingController();
+  
+
+  @override
+  void initState() {
+    // TODO inplement initState
+    if (widget.note != null) {
+      _titleController = TextEditingController(text: widget.note!.title);
+      _contentController = TextEditingController(text: widget.note!.content);
+    }
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
+        child: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  padding: const EdgeInsets.all(0),
+                  icon: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.green.shade800.withOpacity(.8),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Icon(
+                      Icons.arrow_back_ios_new,
+                      color: Colors.white,
+                    ),
+                  ))
+            ],
+          ),
+          Expanded(
+              child: ListView(
+            children: [
+              TextField(
+                controller: _titleController,
+                style: const TextStyle(
+                    color: Color.fromRGBO(255, 255, 255, 1), fontSize: 30),
+                decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Title',
+                    hintStyle: TextStyle(color: Colors.green, fontSize: 30)),
+              ),
+              TextField(
+                controller: _contentController,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+                maxLength: null,
+                decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Type something here',
+                    hintStyle: TextStyle(
+                      color: Colors.green,
+                    )),
+              ),
+            ],
+          ))
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(
+              context, [_titleController.text, _contentController.text]);
+        },
+        elevation: 10,
+        backgroundColor: Colors.green.shade800,
+      ),
+    );
+  }
+  
+}
